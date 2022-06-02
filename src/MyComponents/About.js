c50ec4978937c89a56eb37bc544565c75b2f292d
AdyantB <87541882+AdyantB@users.noreply.github.com>
2022-06-02 21:07:24 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            About
+            <p>
+              <h4 class="card-title">Adyant Bajpai</h4>
+              <p class="card-text">Web Developement Intern</p>
+              <a href="https://www.linkedin.com/in/adyant-bajpai-9bb255217/" class="btn">See Profile</a>
+          </p>
+
+        </div>
+        
+    )
+}
